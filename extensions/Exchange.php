d59d95144a4c102a30f6becc6c6fc4016ca529b4
tolstopuzik <igor.lapteev@gmail.com>
2017-04-19 01:35:53 +0300

    try and catch

--- extensions/Exchange.php
+++ extensions/Exchange.php
@@ -4,45 +4,50 @@ namespace app\extensions;
 
 use app\models as M;
 use Yii;
+use yii\base\ExitException;
 class Exchange
 {
     public function getRates($value, $dateStart, $dateEnd = false) {
-        if(!$value || !$dateStart)
-            return false;
+        try {
+            if (!$value || !$dateStart)
+                return false;
 
-        $cache = Yii::$app->cache;
-        if ( $dateEnd ) {
-            if($cache->exists($value.$dateStart.$dateEnd))
-                return $cache->get($value.$dateStart.$dateEnd);
-            $rates = M\Exchange::find()
-                ->where('date >= :dStart and date <= :dEnd', [':dStart' => $dateStart, ':dEnd' => $dateEnd])
-                ->all();
+            $cache = Yii::$app->cache;
+            if ($dateEnd) {
+                if ($cache->exists($value . $dateStart . $dateEnd))
+                    return $cache->get($value . $dateStart . $dateEnd);
+                $rates = M\Exchange::find()
+                    ->where('date >= :dStart and date <= :dEnd', [':dStart' => $dateStart, ':dEnd' => $dateEnd])
+                    ->all();
 
-            if ($rates) {
+                if ($rates) {
 
-                $res = [];
-                foreach ($rates as $r) {
-                    $res[$r['date']] = ($r['rate'] * $value);
+                    $res = [];
+                    foreach ($rates as $r) {
+                        $res[$r['date']] = ($r['rate'] * $value);
+                    }
+                    $cache->set($value . $dateStart . $dateEnd, $res);
+                    return $res;
                 }
-                $cache->set($value.$dateStart.$dateEnd, $res);
-                return $res;
             }
-        }
 
-        if ( !empty($dateStart) ) {
-            if($cache->exists($value.$dateStart))
-                return $cache->get($value.$dateStart);
+            if (!empty($dateStart)) {
+                if ($cache->exists($value . $dateStart))
+                    return $cache->get($value . $dateStart);
 
-            $rate = M\Exchange::find()
-                ->where('date = :date', [':date' => $dateStart])
-                ->one();
+                $rate = M\Exchange::find()
+                    ->where('date = :date', [':date' => $dateStart])
+                    ->one();
 
-            if ( $rate ) {
-                $res = [$dateStart => ($rate['rate'] * $value)];
-                $cache->set($value.$dateStart.$dateEnd, $res);
-                return $res;
+                if ($rate) {
+                    $res = [$dateStart => ($rate['rate'] * $value)];
+                    $cache->set($value . $dateStart . $dateEnd, $res);
+                    return $res;
+                }
             }
+            return false;
+        } catch (ExitException $e){
+            Yii::error('error');
         }
-        return false;
     }
 }
